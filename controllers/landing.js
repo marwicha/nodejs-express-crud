4bafd866366cab041602bf8c7eba7f0b19d75f84
marwa <marwa.rekik@esprit.tn>
2019-05-06 01:38:23 +0200

    first insertion in database using create method

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,13 @@
+const models = require('../models');
+
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = function(req, res, next) {
-  console.log("lead email: ", req.body.email_lead);
-    res.redirect('/');
+  return models.Lead.create({
+      email: req.body.email_lead
+  }).then(lead => {
+      res.redirect('/');
+  })
 }
